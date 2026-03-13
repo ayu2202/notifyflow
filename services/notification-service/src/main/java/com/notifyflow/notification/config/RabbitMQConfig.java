86e6166862a50c23ab53e7b57bb0650063f8704d
Ayush Trivedi <trivediayush2202@gmail.com>
2026-03-14 04:19:02 +0530

    Improved code sanity

--- services/notification-service/src/main/java/com/notifyflow/notification/config/RabbitMQConfig.java
+++ services/notification-service/src/main/java/com/notifyflow/notification/config/RabbitMQConfig.java
@@ -1,8 +1,6 @@
 package com.notifyflow.notification.config;
 
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
--- services/notification-service/src/main/java/com/notifyflow/notification/repository/package-info.java
+++
@@ -1,7 +0,0 @@
-package com.notifyflow.notification.repository;
-
-/**
- * Placeholder repository package for the Notification Service.
- * The notification service currently does not persist data,
- * but this package is included for structural consistency.
- */
